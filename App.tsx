c72d2c761a83ab22d0ec022fa14e2e8cfca57b83
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-07-02 14:24:23 -0600

    Create basic physics based animation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Text, View, useWindowDimensions } from "react-native";
+import styled from "styled-components/native";
+import useInterval from "@use-it/interval";
+
+const Rocket = styled.View`
+  background-color: red;
+  height: 20px;
+  width: 10px;
+`;
 
 export default function App() {
+  const Force = 10;
+  const Mass = 100;
+  const Acceleration = Force / Mass;
+  const [Time, setTime] = useState(0);
+  const Speed = Acceleration * Time;
+  const Distance = Speed * Time;
+  const { height } = useWindowDimensions();
+  useInterval(() => {
+    setTime(Time + (0.01));
+  }, 10);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        justifyContent: "flex-end",
+        alignItems: "center",
+        height: "100%",
+        width: "100%",
+        bottom: Distance % height,
+        position: "absolute",
+      }}
+    >
+      <Rocket style={{ position: "absolute", bottom: Distance}} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
