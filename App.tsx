ac32daf8cf23aa310457eb755441853635ec4ff3
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-07-02 23:58:57 -0600

    Create simple graph powered by F = MA

--- App.tsx
+++ App.tsx
@@ -3,47 +3,85 @@ import { View, useWindowDimensions } from "react-native";
 import { Svg, Path, Line, G, Text } from "react-native-svg";
 import * as d3 from "d3";
 
-const XAxis = () => {
+const Label = ({ ...props }) => (
+  <Text
+    fill={"black"}
+    stroke={"black"}
+    fontSize={"8"}
+    textAnchor={"middle"}
+    strokeWidth={0.2}
+    {...props}
+  />
+);
+
+const Tick = ({ ...props }) => (
+  <Line stroke={"black"} strokeWidth={1} {...props} />
+);
+
+const XTick = ({ step, maxY }: { step: number; maxY: number }) => (
+  <Tick x1={step} x2={step} y1={maxY} y2={maxY + 5} />
+);
+
+const YTick = ({ step, maxY }: { step: number; maxY: number }) => (
+  <Tick x1={0} x2={-5} y1={maxY - step} y2={maxY - step} />
+);
+
+const Bar = ({ ...props }) => <Line stroke="#000" strokeWidth="1" {...props} />;
+
+const HorizontalBar = ({
+  minX,
+  maxY,
+  maxX,
+}: {
+  minX: number;
+  maxY: number;
+  maxX: number;
+}) => <Bar x1={minX} x2={maxX} y1={maxY} y2={maxY} />;
+
+const VerticalBar = ({
+  minX,
+  maxY,
+  minY,
+}: {
+  minX: number;
+  maxY: number;
+  minY: number;
+}) => <Bar x1={minX} x2={minX} y1={minY} y2={maxY} />;
+
+const XAxis = ({ step = 10, minX = 0, maxX = 100, maxY = 100, unit = "s" }) => {
+  const steps = new Array(Math.floor(maxX / step))
+    .fill("")
+    .map((_, i) => i * step);
   return (
     <>
-      <Line
-        stroke="#000"
-        strokeWidth="3"
-        x1={0}
-        x2={100}
-        y1={100}
-        y2={100}
-      ></Line>
-      <Line stroke="black" strokeWidth="1" x1={10} x2={10} y1={100} y2={105} />
-      <Text
-        fill="#000"
-        stroke="#000"
-        fontSize="8"
-        textAnchor="middle"
-        x={10}
-        y={120}
-      >
-        10
-      </Text>
+      <HorizontalBar minX={minX} maxX={maxX} maxY={maxY} />
+      {steps.map((step) => (
+        <>
+          <XTick step={step} maxY={maxY} />
+          <Label x={step} y={maxY + 20}>
+            {`${step}${unit}`}
+          </Label>
+        </>
+      ))}
     </>
   );
 };
 
-const YAxis = () => {
+const YAxis = ({ step = 10, minY = 0, minX = 0, maxY = 100, unit = "m" }) => {
+  const steps = new Array(Math.floor(maxY / step))
+    .fill("")
+    .map((_, i) => i * step);
   return (
     <>
-      <Line stroke="#000" strokeWidth="3" x1={0} x2={0} y1={0} y2={100}></Line>
-      <Line stroke="black" strokeWidth="2" x1={0} x2={-5} y1={90} y2={90} />
-      <Text
-        fill="#000"
-        stroke="#000"
-        fontSize="8"
-        textAnchor="middle"
-        x={-20}
-        y={90}
-      >
-        10
-      </Text>
+      <VerticalBar maxY={maxY} minX={minX} minY={minY} />
+      {steps.map((step) => (
+        <>
+          <YTick step={step} maxY={maxY} />
+          <Label x={-20} y={maxY - step}>
+            {`${step}${unit}`}
+          </Label>
+        </>
+      ))}
     </>
   );
 };
@@ -54,16 +92,16 @@ const plotPoints = new Array(100).fill("").map((_, Time) => {
   const Acceleration = Force / Mass;
   const Speed = Acceleration * Time;
   const Distance = Speed * Time;
-  return `L ${Time}, ${100 - Distance}`
-})
+  return `L ${Time}, ${100 - Distance}`;
+});
 
 const Graph = () => {
   useEffect(() => {});
   return (
-    <Svg style={{ height: 100, width: 100 }} viewBox="-30 -30 150 150">
+    <Svg style={{ height: 200, width: 200 }} viewBox="-30 -30 150 150">
       <Path d={`M 0,100 ${plotPoints}`} fill="none" stroke={"black"} />
-      <XAxis/>
-      <YAxis/>
+      <XAxis step={20} />
+      <YAxis step={20} />
     </Svg>
   );
 };
