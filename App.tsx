80349b32f24db1982e7fd063acb24268f59961da
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-07-02 21:20:32 -0600

    Remove unused code and packages

--- App.tsx
+++ App.tsx
@@ -11,13 +11,12 @@ const Rocket = styled.View`
 `;
 
 export default function App() {
-  const Force = 10;
-  const Mass = 100;
+  const Force = 100;
+  const Mass = 2;
   const Acceleration = Force / Mass;
   const [Time, setTime] = useState(0);
   const Speed = Acceleration * Time;
   const Distance = Speed * Time;
-  const { height } = useWindowDimensions();
   useInterval(() => {
     setTime(Time + (0.01));
   }, 10);
@@ -28,8 +27,6 @@ export default function App() {
         alignItems: "center",
         height: "100%",
         width: "100%",
-        bottom: Distance % height,
-        position: "absolute",
       }}
     >
       <Rocket style={{ position: "absolute", bottom: Distance}} />
